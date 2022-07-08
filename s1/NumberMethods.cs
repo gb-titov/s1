939749c5b8aff7ac939358cf916b43c652cb3b70
Yury Titov <yuriy-t87@yandex.ru>
2022-07-08 22:36:36 +0300

    Добавьте файлы проекта.

---
+++ s1/NumberMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace s1
+{
+    internal class NumberMethods
+    {
+
+        public static int GetMax(int a, int b)
+        {
+            if (a == b)
+                throw new ArgumentException("Numbers are equal");
+            if (a > b)
+                return a;
+            return b;
+        }
+
+        public static int GetMax(int a, int b, int c)
+        {
+            if (a == b && b == c)
+                throw new ArgumentException("Numbers are equal");
+            if (a > b && a > c)
+                return a;
+            if (b > c)
+                return b;
+            return c;
+        }
+
+        public static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+
+
+        public static IEnumerable<int> GetAllEvenTillNumber(int num)
+        {
+            if (num < 2)
+                throw new ArgumentOutOfRangeException($"Number {num} less than 2");
+
+            List<int> list = new List<int>();
+
+            int i = 2;
+
+            while (i <= num)
+            {
+               
+                list.Add(i);
+                i = i + 2;
+            }
+            return list;
+        }
+    }
+}
---
+++ s1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace s1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нахождение максимального из двух чисел:");
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Максимальное число: {NumberMethods.GetMax(a,b)}" );
+
+
+            Console.WriteLine("Нахождение максимального из трех чисел:");
+            Console.Write("Введите первое число: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Максимальное число: {NumberMethods.GetMax(a, b, c)}");
+
+
+            Console.WriteLine("Проверка на четность:");
+            Console.Write("Введите число: ");
+            a = int.Parse(Console.ReadLine());
+            string isEven = NumberMethods.IsEven(a) ? "четное" : "нечетное";
+
+            Console.WriteLine($"Число {a} {isEven}");
+
+
+            Console.WriteLine("Нахождение всех четных чисел до указанного числа:");
+            Console.Write("Введите число: ");
+            a = int.Parse(Console.ReadLine());
+
+            foreach (var i in NumberMethods.GetAllEvenTillNumber(a))
+            {
+                Console.Write(i + " ");
+            }
+            
+        }
+    }
+}
